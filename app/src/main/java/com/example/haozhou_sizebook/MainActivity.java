bc80ef4d69ab797c177dbd5825d6c0f9d80d8708
HaozhouPang <haozhou@ualberta.ca>
2017-01-22 13:16:44 -0700

    Main page records added
    ListView

--- app/src/main/java/com/example/haozhou_sizebook/MainActivity.java
+++ app/src/main/java/com/example/haozhou_sizebook/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView oldRecord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
